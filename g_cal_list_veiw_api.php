86727a43ce8d2cc05c79e69e574e9960545a0936
Noliani Clemente <noliani.clemente@protonmail.com>
2026-03-16 20:22:09 -0500

    add auth_url route to rest-api

--- g_cal_list_veiw_api.php
+++ g_cal_list_veiw_api.php
@@ -5,19 +5,41 @@ Google Calendar List View
 Author: Noliani Clemente Hernández
 */
 
+require_once WP_PLUGIN_DIR . $GLOBALS[ 'g_cal_list_view_plugin_folder' ] . '/g_cal_list_view_client.php';
+		
+$api_base = 'gclv/v1/';
+$client = new G_Cal_List_View_Client ();
+$client -> init_client ();
+		
+
 /**
  * Add JSON API routes
  */
-add_action( 'rest_api_init', function () {
+add_action( 'rest_api_init', function () use ( $api_base, $client ) {
 	
 	register_rest_route (
-		'gclv/v1/',
+		$api_base,
 		'auth_token',
 		[
 			'methods'  => WP_REST_SERVER::READABLE, // GET
-			'callback' => function ( WP_REST_Request $req ) {
+			'callback' => function ( WP_REST_Request $req ) use ( $client ) {
 				
-				$result = g_cal_list_view_do_refresh_token ( $req );
+				$result = g_cal_list_view_do_refresh_token ( $req, $client );
+				return $result;
+				
+			},
+			'permission_callback' => '__return_true',
+		]
+	);
+	
+	register_rest_route (
+		$api_base,
+		'auth_url',
+		[
+			'methods'  => WP_REST_SERVER::READABLE, // GET
+			'callback' => function ( WP_REST_Request $req ) use ( $client ) {
+				
+				$result = g_cal_list_view_get_auth_url ( $req, $client );
 				return $result;
 				
 			},
@@ -26,10 +48,13 @@ add_action( 'rest_api_init', function () {
 	);
 });
 
+
+
+		
 /**
  * Get Google Authentication Token from Refresh Token
  */
-function g_cal_list_view_do_refresh_token ( WP_REST_Request $req ) {
+function g_cal_list_view_do_refresh_token ( WP_REST_Request $req, $client ) {
 	global $wpdb;
 	
 	$refresh_token_res = $wpdb -> get_row (
@@ -37,10 +62,6 @@ function g_cal_list_view_do_refresh_token ( WP_REST_Request $req ) {
 	);	
 	
 	if ( isset ( $refresh_token_res ) && strlen ( $refresh_token_res -> setting_value ) > 0 ) {
-		require_once WP_PLUGIN_DIR . $GLOBALS[ 'g_cal_list_view_plugin_folder' ] . '/g_cal_list_view_client.php';
-		
-		$client = new G_Cal_List_View_Client ();
-		$client -> init_client ();
 
 		$refresh_token = $refresh_token_res -> setting_value;
 		$auth_token = $client -> get_new_auth_token ( $refresh_token );
@@ -50,4 +71,15 @@ function g_cal_list_view_do_refresh_token ( WP_REST_Request $req ) {
 		return array ( 'status' => 404, 'error' => 'No token' );
 		
 	}
+}
+
+
+
+function g_cal_list_view_get_auth_url ( $req, $client ) {
+	
+	$client -> set_session_state ();
+	$auth_url = $client -> createAuthUrl ();
+	
+	return array ( 'status' => 200, 'url' => $auth_url );
+	
 }
