e3c70df8899512d5a66e5fe6cd5400b76e4305b0
Noliani Clemente <noliani.clemente@protonmail.com>
2026-03-25 10:30:17 -0500

    fix critical bug causing major crash while applying templates
    added validation of the queried object, otherwise the system crashed when a page is assigned as the website's homepage!

--- G_Cal_List_View.php
+++ G_Cal_List_View.php
@@ -293,7 +293,8 @@ add_action('init', 'register_gclv_login_page');
  */
 add_filter( 'template_include', 'gclv_login_template' );
 function gclv_login_template ( $template ) {
-	if ( get_queried_object () -> post_type == 'gclv_login' )
+	$post = get_queried_object ();
+	if ( isset ( $post ) && !is_null ( $post ) && $post -> post_type == 'gclv_login' )
 		return str_replace ( '//', '/', plugin_dir_path(__DIR__) . $GLOBALS[ 'g_cal_list_view_plugin_folder' ] . "/single-gclv_login.php" );
 	else
 		return $template;
